e597bdb223a14caf0fc762891aebf0b171a469ce
Dimitrios.Truchan <mail2dimi@gmail.com>
2023-08-13 22:25:15 +0200

    add first item location endpoint

--- fastapi/find-it-app/main.py
+++ fastapi/find-it-app/main.py
@@ -3,10 +3,17 @@ from fastapi import FastAPI
 app = FastAPI()
 
 ITEMS = [
-    {"title": "Lizettis Frontend", 'author': 'Jimmilony'},
-    {"title": "Lizetti hothot", 'author': 'Dimi'},
-    {"title": "Hello Lizetti", 'author': 'LxD'},
-]
-@app.get("/books")
-async def read_all_books():
-    return ITEMS
+    {"item_name": "Hammer", 'location': 'Spiegelschrank'},
+    {"item_name": "Makis Makakis", 'location': 'Auf der Minibar'},
+    {"item_name": "Lizetti", 'location': 'Couch'},
+    ]
+
+@app.get("/items")
+async def read_all_items():
+    return {ITEMS}
+
+@app.get("/items/{item_name}")
+async def read_item(item_name: str):
+    for item in ITEMS:
+        if item.get('item_name').casefold() == item_name.casefold():
+            return item.get('location')
