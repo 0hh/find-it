dbc6842df74a55abf0da402bf8ba3e44e1a42e28
Dimitrios.Truchan <mail2dimi@gmail.com>
2023-08-13 21:56:03 +0200

    add first endpoint as test

--- fastapi/find-it-app/main.py
+++ fastapi/find-it-app/main.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def first_api():
-    return {"message": "Hello Lizetti"}
+ITEMS = [
+    {"title": "Lizettis Frontend", 'author': 'Jimmilony'},
+    {"title": "Lizetti hothot", 'author': 'Dimi'},
+    {"title": "Hello Lizetti", 'author': 'LxD'},
+]
+@app.get("/books")
+async def read_all_books():
+    return ITEMS
