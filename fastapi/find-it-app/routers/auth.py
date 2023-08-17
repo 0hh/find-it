378e009bad851cf91e8a6a1f2f29f1614a15de94
Dimitrios.Truchan <mail2dimi@gmail.com>
2023-08-17 07:58:38 +0200

    implement user authentication endpoint, user can log on with password

--- fastapi/find-it-app/routers/auth.py
+++ fastapi/find-it-app/routers/auth.py
@@ -6,6 +6,7 @@ from database import SessionLocal
 from models import Users
 from passlib.context import CryptContext
 from starlette import status
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter()
 
@@ -35,7 +36,14 @@ def get_db():
         db.close()
 
 db_dependency = Annotated[Session, Depends(get_db)]
-db: Annotated[Session, Depends(get_db)]
+
+def authentcate_user(username: str, password: str, db):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return True
 
 @router.post("/auth", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency,
@@ -50,3 +58,12 @@ async def create_user(db: db_dependency,
     )
     db.add(create_user_model)
     db.commit()
+
+
+@router.post("/token")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                                 db: db_dependency):
+    user = authentcate_user(form_data.username, form_data.password, db)
+    if not user:
+        return 'Failed Authentication'
+    return 'Successful Authentication'
